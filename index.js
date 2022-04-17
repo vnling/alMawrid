fd7da6c71de095d1751adeac294a4001d782305b
Hanzalla Usman <hu297@nyu.edu>
2022-04-17 17:13:00 +0400

    Added 3D feature

--- index.js
+++ index.js
@@ -9,13 +9,62 @@ const map = new mapboxgl.Map({
   // Amman, Jordan [E, N]
   center: [35.930359, 31.963158],
   zoom: 9, // starting zoom
+  pitch: 45,
+  bearing: -17.6,
 });
 
 map.on("load", () => {
+  // Insert the layer beneath any symbol layer.
+  const layers = map.getStyle().layers;
+  const labelLayerId = layers.find(
+    (layer) => layer.type === "symbol" && layer.layout["text-field"]
+  ).id;
+
+  // The 'building' layer in the Mapbox Streets
+  // vector tileset contains building height data
+  // from OpenStreetMap.
+  map.addLayer(
+    {
+      id: "add-3d-buildings",
+      source: "composite",
+      "source-layer": "building",
+      filter: ["==", "extrude", "true"],
+      type: "fill-extrusion",
+      minzoom: 13,
+      paint: {
+        "fill-extrusion-color": "#aaa",
+
+        // Use an 'interpolate' expression to
+        // add a smooth transition effect to
+        // the buildings as the user zooms in.
+        "fill-extrusion-height": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          15,
+          0,
+          15.05,
+          ["get", "height"],
+        ],
+        "fill-extrusion-base": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          15,
+          0,
+          15.05,
+          ["get", "min_height"],
+        ],
+        "fill-extrusion-opacity": 0.6,
+      },
+    },
+    labelLayerId
+  );
   map.addSource("places", {
     // This GeoJSON contains features that include an "icon"
     // property. The value of the "icon" property corresponds
     // to an image in the Mapbox Streets style's sprite.
+
     type: "geojson",
     data: {
       type: "FeatureCollection",
